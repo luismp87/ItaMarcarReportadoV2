2ed190b087b6aff9e415feb4c246f9caa1e8f019
luismp87 <luismp87@gmail.com>
2018-03-07 17:10:25 -0600

    ss
    ss

--- ItaMarcarReportadoV2/js/actions.js
+++ ItaMarcarReportadoV2/js/actions.js
@@ -130,7 +130,7 @@ var fn = {
                                 $("#pPART_DESC").text(msg[i].part_desc);
                                 $("#pLI_MARCA").text(msg[i].li_marca);
                                 $("#pLI_MODELO").text(msg[i].li_modelo);
-                                $("#pLI_SERIE").text(msg[i].li_serie);
+                                $("#pLI_SERIE").text(msg[i].li_serie);                                
                                 $("#pOBSERVACIONES").text(msg[i].observaciones);
                                 $("#pIP_NOMBRE_DTO").text(msg[i].ip_nombre_dto);
                                 $("#pNUMERO_EMPLEADO").text(msg[i].numero_empleado);
@@ -354,6 +354,7 @@ var fn = {
                                 $("#pLI_MARCA").text(msg[i].li_marca);
                                 $("#pLI_MODELO").text(msg[i].li_modelo);
                                 $("#pLI_SERIE").text(msg[i].li_serie);
+                                $("#pOBSERVACIONES").text(msg[i].observaciones);
                                 $("#pIP_NOMBRE_DTO").text(msg[i].ip_nombre_dto);
                                 $("#pNUMERO_EMPLEADO").text(msg[i].numero_empleado);
                                 $("#pIP_NOMBRE_EMPLEADO").text(msg[i].ip_nombre_empleado);
